755213bf0ba433cef65444f9610d6cecf2b8bd1a
Samuele Converso <samu.converso@gmail.com>
2025-03-03 17:13:31 +0100

    Aggiunta chiamata Get per homepage; messo appsettings in gitignore

--- BUILD-WEEK-4_TEAM-7/Controllers/HomeController.cs
+++ BUILD-WEEK-4_TEAM-7/Controllers/HomeController.cs
@@ -1,11 +1,49 @@
 using System.Diagnostics;
 using BUILD_WEEK_4_TEAM_7.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 
 namespace BUILD_WEEK_4_TEAM_7.Controllers {
+    [AutoValidateAntiforgeryToken]
     public class HomeController : Controller {
-        public IActionResult Index() {
-            return View();
+        private readonly string _connectionString;
+
+        public HomeController() {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false, true)
+                .Build();
+
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public async Task<IActionResult> Index() {
+            var productsList = new ProductsViewModel() {
+                Products = new List<Product>()
+            };
+
+            await using (SqlConnection connection = new SqlConnection(_connectionString)) {
+                await connection.OpenAsync();
+                string query = "SELECT * FROM Products";
+
+                await using (SqlCommand command = new SqlCommand(query, connection)) {
+                    await using (SqlDataReader reader = await command.ExecuteReaderAsync()) {
+                        while (await reader.ReadAsync()) {
+                            productsList.Products.Add(
+                                new Product() {
+                                    IdProduct = reader.GetGuid(0),
+                                    ProductName = reader.GetString(1),
+                                    //Category = reader.GetString(2),
+                                    Price = reader.GetDecimal(4),
+                                    Description = reader.GetString(2)
+                                }
+                            );
+                        }
+                    }
+                }
+            }
+
+            return View(productsList);
         }
 
         public IActionResult Privacy() {
