82910a03432ad92bcf4eb8f98f9071428c31c6f9
Samuele Converso <samu.converso@gmail.com>
2025-03-03 09:57:45 +0100

    Aggiungere i file di progetto.

---
+++ BUILD-WEEK-4_TEAM-7/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BUILD_WEEK_4_TEAM_7.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
