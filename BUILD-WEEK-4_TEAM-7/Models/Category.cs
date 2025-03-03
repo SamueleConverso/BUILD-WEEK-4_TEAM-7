0e2537991446b79ae918678bd3ef275b92d02f12
Samuele Converso <samu.converso@gmail.com>
2025-03-03 12:36:36 +0100

    Creato model per Category e aggiunta proprietà Category nel model Product

---
+++ BUILD-WEEK-4_TEAM-7/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace BUILD_WEEK_4_TEAM_7.Models {
+    public class Category {
+        public int IdCategory {
+            get; set;
+        }
+        public string CategoryName {
+            get; set;
+        }
+    }
+}
