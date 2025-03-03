46fdb98094742c08e8ff47b669614ee49856651d
Reyett___ <reyrenna4@gmail.com>
2025-03-03 17:15:25 +0100

    add + admin visual

---
+++ BUILD-WEEK-4_TEAM-7/Models/CategoryViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace BUILD_WEEK_4_TEAM_7.Models
+{
+    public class CategoryViewModel
+    {
+        public List<Category>? Categories
+        {
+            get; set;
+        }
+    }
+}
