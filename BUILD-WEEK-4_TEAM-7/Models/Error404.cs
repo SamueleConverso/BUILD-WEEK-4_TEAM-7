0386677f604dcaf1e76ffa4c5cbb948e766520b9
Tommasodibertomancini <tommydbm@gmail.com>
2025-03-07 10:11:32 +0100

    Page 404

---
+++ BUILD-WEEK-4_TEAM-7/Models/Error404.cs
@@ -0,0 +1,9 @@
+﻿namespace BUILD_WEEK_4_TEAM_7.Models
+{
+    public class Error404
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
